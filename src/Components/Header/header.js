7f1dfe71961106617ce3db61352df1fe3af6d50b
Alma Powell <alma111000@gmail.comgit config --global user.email alma111000@gmail.comgit config --global user.name Alma>
2019-07-19 16:16:38 -0600

    finishing the header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import miniLogo from '../../Image/noun_Airplane_9163.png'
+import largeLogo from '../../Image/fluberlogo.png'
+import '../../App.css'
 
 
 export default function header() {
     return (
         <div style={styles}>
-            <Link style={font} to="/">Home</Link>
-            <Link style={font} to="/search">Search</Link>
+            {/* Used <a> tag to link logo to Landing page -----Alma */}
+            <a href="/"><div>
+                <img className="largeLogo" src={largeLogo} alt="large logo" />
+                <img className="miniLogo" src={miniLogo} alt="large logo" />
+            </div></a>
+            <div>
+                <Link style={font} to="/">Home</Link>
+                <Link style={font} to="/search">Search</Link>
+            </div>
         </div>
     )
 }
@@ -17,15 +27,16 @@ const styles = {
     backgroundColor: '#0079BF',
     marginRight: '20px',
     display: 'flex',
-    justifyContent: 'flex-end',
+    justifyContent: 'space-between',
     alignItems: 'center',
     position: 'sticky',
-    top: '0'
+    top: '0',
+
 }
 
 const font = {
     textDecoration: 'none',
-    fontSize: '30px',
+    fontSize: '1.5em',
     color: '#c0c0c0',
     margin: '20px',
     fontFamily: 'Rockwell'
