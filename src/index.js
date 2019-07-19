0d494abcaad847f944ffb645d7eaf0d6ab5c68b2
TylerT <alpinetyler@gmail.com>
2019-07-19 13:44:30 -0600

    git making me do this

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { HashRouter as Router } from 'react-router-dom';
 
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
