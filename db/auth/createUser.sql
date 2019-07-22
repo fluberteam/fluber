ad01eacd73339834d0e224617be05cf2a61c7346
Todd Hipps <toddhipps@Todds-MacBook-Pro.local>
2019-07-22 11:32:58 -0600

    just completed the login scripts

---
+++ db/auth/createUser.sql
@@ -0,0 +1,31 @@
+insert into users
+(
+first_name,
+last_name,
+email,
+password,
+status,
+address1,
+address2,
+city,
+state,
+zipcode,
+phone,
+operator_num,
+)
+values (
+${first_name},
+${last_name},
+${email},
+${password},
+${status},
+${address1},
+${address2},
+${city},
+${state},
+${zipcode},
+${phone},
+${operator_num}
+)
+
+returning email, status;
