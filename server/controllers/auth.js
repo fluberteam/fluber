ebb9b6c55b174c3a66de354d23dfe138afe880e5
TylerT <alpinetyler@gmail.com>
2019-07-22 11:35:09 -0600

    finished auth controller and enpoints in index.js

---
+++ server/controllers/auth.js
@@ -0,0 +1,114 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+    register: async (req, res) => {
+        //user inputs data: name, email, password
+        //check if email is in db // send 409 status
+        //create a salt
+        //hash password & salt
+        //store name, email, hash into table
+
+        try {const db = req.app.get('db')
+        const {name, email, password, status, certnumb} = req.body
+
+        let users = await db.findUserByEmail(email)
+        let user = users[0]
+
+        if(user) {
+            return res.status(409).send('email already in db')
+        }
+
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password, salt)
+
+       let response = await db.createUser(name, email, hash, isadmin, rentalid)
+       let newUser = response[0]
+
+       delete newUser.password
+
+       req.session.user = newUser
+       res.send(req.session.user)
+    
+        } catch (error) {
+            console.log('there was an error')
+            res.status(500).send(error)
+        }
+
+        
+    },
+
+    login: async (req, res) => {
+        // user input: email, password
+        // get user by email from db
+        // if no user send 401 status
+        // compare password with hash using bcrypt
+        // if they don't match send 401 status
+        // if they match, add user to session
+        try {const db = req.app.get('db')
+        const {email, password} = req.body
+
+
+        let users = await db.auth.findUserByEmail(email)
+        let user = users[0]
+
+      
+
+        if (!user) {
+            return res.status(401).send('email or password incorrect')
+        }
+
+        let isAuthenticated = bcrypt.compareSync(password, user.password)
+
+        if (!isAuthenticated) {
+            return res.status(401).send('email or password incorrect')
+        }
+
+        delete user.password
+        req.session.user = user
+        res.send(req.session.user)
+
+        // console.log(req.session.user.name, 'logged in')
+    
+    } catch (error) {
+        console.log('there was an error', error)
+        res.status(500).send(error)
+    }
+        
+
+    }, 
+
+    logout: (req, res) => {
+        req.session.destroy()
+        res.sendStatus(200)
+    }, 
+
+    currentUser: (req, res) => {
+        res.send(req.session.user)
+        
+    },
+
+    updateUser: async (req, res) => {
+        try {
+            const db = req.app.get('db')
+            const { name, email } = req.body
+            let { id } = req.params
+            id = +id
+            let foundUser = await db.auth.findUserById([id])
+            let userName = await db.auth.updateUserName({
+                id,
+                name: name ? name : foundUser[0].name,
+                email: email ? email : foundUser[0].email
+            })
+            let user = userName[0]
+            delete user.password
+            req.session.user = user
+            res.status(200).send(req.session.user)
+        } catch (error) {
+            console.log('there was an error updating', error)
+            res.status(500).send(error)
+        }
+    }
+ 
+
+
+}
