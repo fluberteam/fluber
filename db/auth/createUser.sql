0f2b5a89b0321d98a21638406cf940941549a820
TylerT <alpinetyler@gmail.com>
2019-07-22 20:06:31 -0600

    updated createUser.sql, auth.js, index.js, commented out Login on App.js

--- db/auth/createUser.sql
+++ db/auth/createUser.sql
@@ -11,21 +11,21 @@ city,
 state,
 zipcode,
 phone,
-operator_num,
+operator_num
 )
 values (
-${first_name},
-${last_name},
-${email},
-${password},
-${status},
-${address1},
-${address2},
-${city},
-${state},
-${zipcode},
-${phone},
-${operator_num}
+$1,
+$2,
+$3,
+$4,
+$5,
+$6,
+$7,
+$8,
+$9,
+$10,
+$11,
+$12
 )
 
 returning email, status;
