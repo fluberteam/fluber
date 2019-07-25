9388a80c7a01bdcd3da2f38b2aab4593e2359b5f
Grant Fox <granttfox@gmail.com>
2019-07-25 15:34:32 -0600

    added to the search controller

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -1,9 +1,12 @@
 module.exports = {
     getAll: async (req, res) => {
         try {
-           const db = req.app.get('db') 
+           const db = req.app.get('db')
+           let searchedFlights = await db.search.firstSearch() 
+           res.status(200).send(searchedFlights)
         } catch (error) {
-            
+            console.log('error getting searched flights', error)
+            res.status(500).send(error)
         }
     }
 }
