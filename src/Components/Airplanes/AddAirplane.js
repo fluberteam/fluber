7005f049711c2cd1a5e4a5486d92d4a94c3320d3
TylerT <alpinetyler@gmail.com>
2019-07-26 10:52:09 -0600

    finished full CRUD for airplanes

--- src/Components/Airplanes/AddAirplane.js
+++ src/Components/Airplanes/AddAirplane.js
@@ -1,16 +1,162 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, {useState} from 'react'
+import {connect} from 'react-redux'
 import { createAirplane } from '../../Redux/Reducers/airplanes'
+import {Redirect} from 'react-router-dom'
+import { StyledInput } from '../StyledComps/StyledInput'
+import {LoginForm} from '../Login/StyledLogin'
+import largeLogo from '../../Image/fluberlogo.png'
 
 
+const AddAirplane = props => {
 
-function AddAirplane() {
+    let [state, setState] = useState({
+        n_number: '',
+        certnumber: '',
+        img: '',
+        engine_id: '',
+        num_seats: '',
+        restroom: '',
+        flight_attn: '',
+        refreshments: '',
+        cruise_speed: ''
+    })
+
+    const [addAirplane, setaddAirplane] = useState(false)
+
+    const handleChange = e => {
+        let {name, value} = e.target
+        setState({ ...state, [name]: value })
+    }
+
+    const handleSubmit = () => {
+        props.createAirplane(state)
+        toggle()
+    }
+
+    const toggle = () => {
+        setaddAirplane(!addAirplane)
+    }
+
+
     return (
-        <div style={styles.admin}>
-            Add Airplane
+        <>
+        {addAirplane?
+        <div style={styles.div}>
+        <LoginForm>
+            <img src={largeLogo} alt="Large Logo" style={{height: '100px'}}/>
+            <StyledInput
+                type='text'
+                name='n_number'
+                placeholder='N-Number'
+                onChange={handleChange} />
+           
+            <StyledInput
+                type='text'
+                name='certnumber'
+                placeholder='Operator Number'
+                onChange={handleChange} />
+            
+            <StyledInput
+                type='text'
+                name='img'
+                placeholder='Image'
+                onChange={handleChange} />
+            
+            <select
+                type='text'
+                name='engine_id'
+                onChange={handleChange}>
+                <option>Choose Engine Type</option>  
+                <option value="SE">SE Turboprop</option>
+                <option value="ME">ME Turboprop</option>
+                <option value="TJ">Turbojet</option>
+            </select>
+
+            <StyledInput
+                type='text'
+                name='num_seats'
+                placeholder='Number of Seats'
+                onChange={handleChange} />
+            
+            <span style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+          <span>
+              <span>Restroom?   </span>
+           <input
+                type='radio'
+                name='restroom'
+                value='true'
+                onChange={handleChange}/>Yes
+            </span>
+            <span>
+            <input
+                type='radio'
+                name='restroom'
+                value='false'
+                onChange={handleChange}/>No 
+            </span>
+            </span>
+            <span style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+          <span>
+              <span>Flight Attendant?   </span>
+           <input
+                type='radio'
+                name='flight_attn'
+                value='true'
+                onChange={handleChange}/>Yes
+            </span>
+            <span>
+            <input
+                type='radio'
+                name='flight_attn'
+                value='false'
+                onChange={handleChange}/>No 
+            </span>
+            </span>
+            <span style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+          <span>
+              <span>Refreshments?   </span>
+           <input
+                type='radio'
+                name='refreshments'
+                value='true'
+                onChange={handleChange}/>Yes
+            </span>
+            <span>
+            <input
+                type='radio'
+                name='refreshments'
+                value='false'
+                onChange={handleChange}/>No 
+            </span>
+            </span>
+           
+            <StyledInput
+                type='text'
+                name='cruise_speed'
+                placeholder='Cruise Speed'
+                onChange={handleChange} />
+            
+            
+            <button onClick={handleSubmit}>Add Airplane</button>
+            <button className="addbutton" onClick={toggle}>Cancel</button>
+
+
+        </LoginForm>
         </div>
-    )
-}
+        
+        
+        :
+        <button>
+            <section>
+                <p><button style={styles.addbutton} onClick={toggle}>Add Airplane</button></p>
+            </section>
+        
+        </button>
+        }
+        </>
+        )
+
+    }
 
 
 const mapStateToProps = state => {
@@ -32,6 +178,10 @@ let styles = {
         flexDirection: 'column ',
         textDecoration: 'none',
         fontFamily: 'Rockwell'
+    },
+    addbutton: {
+        border: 'none',
+        
     }
 }
 
