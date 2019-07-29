61520f7fa7bcdc568e9a6907172ca8eaf9a1836d
Grant Fox <granttfox@gmail.com>
2019-07-29 15:06:02 -0600

    added functionality and search response fields

---
+++ src/Components/Search/StyledSearch.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const StyledSearch = styled.div`
+
+    display: flex;
+    height: 25%
+    width: content;
+    z-index: 0;
+    flex-direction: column;
+    background: white;
+    border-radius: 6px;
+    border: 1px solid #c0c0c0;
+    box-shadow: 5px 10px 18px #888888;
+    padding: 10px;
+    height: auto;
+    justify-content: center;
+    align-items: space-between;
+
+
+`
