34b1c831fbbdd105c34809c2c1c01d6e471acfdc
Todd Hipps <toddhipps@Todds-MBP.lan>
2019-07-29 14:01:42 -0600

    Todd working on dashboards

--- src/Components/Operator/OperatorDashboard.js
+++ src/Components/Operator/OperatorDashboard.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 export default function OperatorDashboard() {
     return (
-        <div style={styles.admin}>
-            Operator Dashboard
-        </div>
+        <div style={styles.admin} className="reports">
+                <h2>Operator Dashboard</h2><br></br>
+                <Link to={'/ScheduledFlights'}>
+                    <h3><span style={styles.icon} className="fa">&#xf0e0; </span>   Scheduled Flights</h3>
+                </Link>
+                <Link to={'/Airplanes'}>
+                    <h3><span style={styles.icon} className="fa">&#xf007; </span>  Airplanes</h3>
+                </Link>
+                <Link to={'/Payments'}>
+                    <h3><span style={styles.icon} className="fa">&#xf155; </span>  Payment List</h3>
+                </Link>
+                
+                
+                
+                
+            </div>
     )
 }
 
@@ -16,7 +31,10 @@ let styles = {
         justifyContent: 'center',
         flexDirection: 'column ',
         textDecoration: 'none',
-        fontFamily: 'Rockwell'
+        fontFamily: ''
+    },
+    icon: {
+        fontSize: '25px',
+        textDecoration: 'none'
     }
 }
-
